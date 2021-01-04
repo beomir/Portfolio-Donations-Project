aae049133feca63594cff5ad352cdd032c267b0b
Andrzej <beomir89@gmail.com>
2021-01-04 11:03:40 +0100

    Sending confirmation mail - contact form

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -61,7 +61,7 @@ public class HomeController {
 
     @PostMapping("contactForm")
     public String contactForm( String message, String name, String surname, String email) {
-        sendEmailService.sendEmailFromContactForm("<b>Message:</b><i> " + message + "</i>, <b>name: </b><i>" + name + "</i>,<b> surname:</b><i> " + surname + "</i>, <b>email:</b> " + email);
+        sendEmailService.sendEmailFromContactForm("<span style=\"font-size: 16px;\"><b>Name: </b><i>" + name + "</i><br/><b> Surname:</b><i> " + surname + "</i><br/><b>Email:</b> " + email + "<br/><br/><b>Message:</b><i></span> " + message + "</i>");
         sendEmailService.sendEmail(email,"Witaj " + name + " " + surname + ", otrzymaliśmy od Ciebię prośbę o kontakt. Skontaktujemy sie z Tobą tak szybko jak będzie to możliwe","Potwierdzenie prośby o kontakt");
         return "redirect:/contactForm-confirmation";
     }
--- src/main/java/pl/coderslab/charity/controller/UsersController.java
+++ src/main/java/pl/coderslab/charity/controller/UsersController.java
@@ -1,8 +1,6 @@
 package pl.coderslab.charity.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -74,7 +72,7 @@ public class UsersController {
         model.addAttribute("registrationStatus", usersService.registrationStatus());
         return "register-confirmation";
     }
-    
+
 
     @GetMapping("/login")
     public String login(Model model) {
